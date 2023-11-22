431aaf2e5980f24799e12c7af9f5555e085770f6
Madeline Coco <madelinecoco@Madelines-MacBook-Pro.local>
2023-11-21 21:44:56 -0500

    updates

--- wordsearch_3b/main.cpp
+++ wordsearch_3b/main.cpp
@@ -24,13 +24,196 @@ void findMatches(Dictionary& dict, Grid& grid)
     for (int r = 0; r < grid.getRows(); r++) {
         for (int c = 0; c < grid.getCols(); c++) {
 
-            // Multiple code blocks here search for words in various directions (up, down, diagonally, etc.)
-            // Each block sets the starting position and iterates through the grid, checking for words
-            // that meet the length criteria and exist in the dictionary
+            // Search directly up
+            // Set row and col to current row and col
+            row = r, col = c;
+            //Initialize word to be empty
+            word = ""; 
+            //Add up to the maxLength of characters
+            for (l = 0; l <= maxLength; l++) {
+                word += matrix[row][col];   
+                if (word.length() >= minLength) { 
+                    //Look up the word and if it exists, print it out
+                    if (dict.lookupWord(word)) {
+                        cout << word << endl;
+                    }
+                }
+                //Iterate until at the edge, then wrap
+                if (col == 0) {
+                    col = grid.getCols() - 1;
+                } else {
+                    col--;
+                }
+            } 
+            //End searching up
 
-            // ...
-            // Rest of the code for other search directions
-            // ...
+            //Search up and right (diagonal)
+            row = r, col = c;
+            //Initialize word to be empty
+            word = "";
+            for (l = 0; l <= maxLength; l++) {
+                //Add on the current character
+                word += matrix[row] [col];
+                //Look up the word and if it exists, print it out
+                if (word.length() >= minLength) {
+                    if (dict.lookupWord(word)) {
+                        cout << word << endl;
+                    }
+                }
+                //Iterate until at the edge, then wrap
+                if (col == 0) {
+                    col = grid.getCols() - 1;
+                } else {
+                    col--;
+                }
+                if (row == grid.getRows() - 1) {
+                    row = 0;
+                } else {
+                    row++;
+                }
+            }
+            //End search up and right
+
+            //Search to the right
+            row = r, col = c; 
+            //Initialize word to be empty
+            word = ""; 
+            for (l = 0; l <= maxLength; l++) {
+                word += matrix[row][col];
+                //Look up the word and if it exists, print it out
+                if (word.length() >= minLength) {
+                    if (dict.lookupWord(word)) {
+                        cout << word << endl;
+                    }
+                }
+                //Iterate until at the edge, then wrap
+                if (row == grid.getRows() - 1) {
+                    row = 0;
+                } else {
+                    row++;
+                }
+            } 
+            //End search to the right
+
+            //Search down and right (diagonal)
+            row = r, col = c;
+            word = "";
+            for (l = 0; l <= maxLength; l++) {
+                word += matrix[row][col];
+
+                //Look up the word and if it exists, print it out
+                if (word.length() >= minLength) {
+                    if (dict.lookupWord(word)) {
+                        cout << word << endl;
+                    }
+                }
+                //Iterate until at the edge, then wrap
+                if (col == grid.getCols() - 1) {
+                    col = 0;
+                } else {
+                    col++;
+                }
+                if (row == grid.getRows() - 1) {
+                    row = 0;
+                } else {
+                    row++;
+                }
+            } //End search down and right
+
+            //Search directly down
+            row = r, col = c;
+            word = "";
+
+            for (l = 0; l <= maxLength; l++) 
+            {
+                word += matrix[row][col];
+
+                //Look up the word and if it exists, print it out
+                if (word.length() >= minLength) {
+                    if (dict.lookupWord(word)) {
+                        cout << word << endl;
+                    }
+                }
+                //Iterate until at the edge, then wrap
+                if (col == grid.getCols() - 1) {
+                    col = 0;
+                } else {
+                    col++;
+                }
+            } //End search down
+
+            //Search down and left (diagonal)
+            row = r, col = c; 
+            word = "";
+            for (l = 0; l <= maxLength; l++) {
+                word += matrix[row][col];
+
+                //Look up the word and if it exists, print it out
+                if (word.length() >= minLength) {
+                    if (dict.lookupWord(word)) {
+                        cout << word << endl;
+                    }
+                }
+                //Iterate until at the edge, then wrap
+                if (col == grid.getCols() - 1) {
+                    col = 0;
+                } else {
+                    col++;
+                }
+                if (row == 0) {
+                    row = grid.getRows() - 1;
+                } else {
+                    row--;
+                }
+            } //End search down and left
+
+            //Search to the left
+            row = r, col = c; 
+            word = "";
+
+            for (l = 0; l <= maxLength; l++) {
+                word += matrix[row][col];
+
+                //Look up the word and if it exists, print it out
+                if (word.length() >= minLength) {
+                    if (dict.lookupWord(word)) {
+                        cout << word << endl;
+                    }
+                }
+                //Iterate until at the edge, then wrap
+                if (row == 0) {
+                    row = grid.getRows() - 1;
+                } else {
+                    row--;
+                }
+            } //End search left
+
+            //Search up and left (diagonal)
+            row = r, col = c;
+            word = "";
+
+            for (l = 0; l <= maxLength; l++) {
+                word += matrix[row][col];
+
+                //Look up the word and if it exists, print it out
+                if (word.length() >= minLength) {
+                    if (dict.lookupWord(word)) {
+                        cout << word << endl;
+                    }
+                }
+                //Iterate until at the edge, then wrap
+                if (col == 0) {
+                    col = grid.getCols() - 1;
+                }
+                else {
+                    col--;
+                }
+                if (row == 0) {
+                    row = grid.getRows() - 1;
+                } else {
+                    row--;
+                }
+            } //End search up and left
         }
     }
 }
@@ -48,11 +231,13 @@ void search(int algorithm)
 
     // Select sorting algorithm based on input
     if (algorithm == 1) {
+        cout << "Using selection sort..." << endl << endl;
         dict.selectionSort();
     } else if (algorithm == 2) {
         cout << "Using quick sort..." << endl << endl;
         dict.quickSort(0, dict.getSize() - 1);
     } else if (algorithm == 3) {
+        cout << "Using heap sort..." << endl << endl;
         dict.heapSort();
     } else {
         cout << "Input a number 1-3 for sorting options." << endl;
